b4164e6a2b6e6c705d4d0a43def2768deefe2563
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2019-02-21 21:09:00 +0100

    add navbar

---
+++ src/components/organisms/Navbar/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+class Navbar extends React.Component {
+    render () {
+        return (
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        <h1>Get Lunch</h1>
+                    </a>
+
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
+                    href="/">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                        <a className="button is-primary" href="/">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a className="button is-light" href="/">
+                            Log in
+                        </a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+export default Navbar;
